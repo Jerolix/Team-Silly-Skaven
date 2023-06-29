b7054e51affc47d7c31199bfc1ca17621a69a7d7
Krixpp <131213803+Krixpp@users.noreply.github.com>
2023-06-29 16:36:11 +1000

    Tarek - Commit 8: Crossbow VFX
    - Added Reload animation to crossbow
    - Added Firing animation to crossbow
    - Added flying bolt effect to reload animation

--- UnityProj/Assets/Scrips/crossBow.cs
+++ UnityProj/Assets/Scrips/crossBow.cs
@@ -7,13 +7,51 @@ public class crossBow : MonoBehaviour
     public Transform boltSpawnPoint;
     public GameObject boltPrefab;
     public float boltSpeed = 10;
+    [SerializeField] private bool canFire;
+    [SerializeField] private Animator bowAnimator;
+
+    private void Start()
+    {
+        if (canFire == false)
+        {
+            canFire = true;
+        }
+    }
 
     void Update()
     {
+       /* if (Input.GetMouseButtonDown(0))
+        {
+            if (canFire == true)
+            {
+                {
+                    bowAnimator.SetTrigger("Retracting");
+                }
+            }
+        }*/
         if (Input.GetMouseButtonUp(0))
         {
-            var Bolt = Instantiate(boltPrefab, boltSpawnPoint.position, boltSpawnPoint.rotation);//gets the bolt to appear and where it should appear, the origin point of the crossbow
-            Bolt.GetComponent<Rigidbody>().velocity = boltSpawnPoint.forward * boltSpeed;//get bolt to move.
+            if (canFire == true) 
+            {   
+                canFire= false;
+
+                bowAnimator.SetTrigger("Fired");
+                var Bolt = Instantiate(boltPrefab, boltSpawnPoint.position, boltSpawnPoint.rotation);//gets the bolt to appear and where it should appear, the origin point of the crossbow
+                Bolt.GetComponent<Rigidbody>().velocity = boltSpawnPoint.forward * boltSpeed; //get bolt to move.
+
+                StartCoroutine(Cooldown());
+            }
         }
     }
+
+
+    IEnumerator Cooldown()
+    {
+        print("RETRACTING");
+        bowAnimator.SetTrigger("Retracting");
+        yield return new WaitForSeconds(1.25f);
+        //bowAnimator.SetTrigger("Idle");
+        canFire = true;
+    }
+
 }
