6d9b9728d92d01394e0be5a5caacc1812a92888f
Krixpp <131213803+Krixpp@users.noreply.github.com>
2023-06-29 14:27:59 +1000

    Tarek - Commit 7: Bolt Rework
    - Crossbow Bolts will now stick to objects that collide with them.
    - Updated Brainstorming document (checked off 'to do' list)

--- UnityProj/Assets/Scrips/boltStatus.cs
+++ UnityProj/Assets/Scrips/boltStatus.cs
@@ -12,12 +12,38 @@ public class boltStatus : MonoBehaviour
     }
 
 
-    void OnCollisionEnter(Collision gameObject)
+    void OnCollisionEnter(Collision Collider)
     {
-        if (isHit == false)
+        if (isHit == false && Collider.gameObject.tag != "Prop")
         {
             isHit = true;
+            OnTouched();
             print("Hit");
         }
+        if (isHit == false && Collider.gameObject.tag == "Prop")
+        {
+            transform.parent = Collider.gameObject.transform;
+            isHit = true;
+            StartCoroutine(HitProp());
+            print("Hit Prop");
+        }
     }
+
+    IEnumerator HitProp()
+    {
+        yield return new WaitForSeconds(0.02f);
+        gameObject.GetComponent<MeshCollider>().enabled = false;
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        Destroy(gameObject, 15);
+
+    }
+    void OnTouched()
+    {
+        gameObject.GetComponent<MeshCollider>().enabled = false;
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        Destroy(gameObject, 15);
+    }
+
 }
