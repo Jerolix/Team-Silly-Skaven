df0fc715c799a8443f21d42f52645963f0aa036a
Krixpp <131213803+Krixpp@users.noreply.github.com>
2023-07-20 16:59:50 +1000

    Tarek - Commit 27: Targets hit timer
    - Added a UI timer that counts (in seconds) indefinitely until all targets have been hit
    - Text turns green when timer ends

--- UnityProj/Assets/Scrips/hitCounter.cs
+++ UnityProj/Assets/Scrips/hitCounter.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class hitCounter : MonoBehaviour
 {
     public int targetsHit = 0;
+    public int maxTargets;
     public TextMeshProUGUI scoreText;
     public Animator counterAnimator;
+    public TextMeshProUGUI timerText;
+
+    public float currentTime = 0;
+
+    private void Start()
+    {
+        StartCoroutine(Timer());
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,6 +27,26 @@ public class hitCounter : MonoBehaviour
         {
             counterAnimator.SetTrigger("ValueChanged");
             scoreText.text = "Targets Hit: " + targetsHit + "/17";
+        }
+    }
+
+
+    IEnumerator Timer()
+    {
+        while (true) 
+        {
+            if (targetsHit < maxTargets)
+            {
+                yield return new WaitForSeconds(1.0f);
+                currentTime += 1.0f;
+                timerText.text = "Time: " + currentTime.ToString() + "s";
+            }
+            else if (targetsHit >= maxTargets)
+            {
+                timerText.color = Color.green;
+                StopCoroutine(Timer());
+                break;
+            }
         }
     }
 }
